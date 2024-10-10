bb57e9a17fdb8ed79d331294d1990cb064381e91
Juho S <juho.salli@gmail.com>
2024-10-10 13:52:19 +0300

    lisää muututjatluennot

---
+++ luennot/Muuttujat/Program.cs
@@ -0,0 +1,27 @@
+﻿int age = int.MaxValue;
+
+// age = 23;
+
+Console.WriteLine(age);
+
+
+float pi = 3.14f;
+
+// double pi2 = 3.14;
+Console.WriteLine(pi);
+
+string name = "samk";
+string rnd_str = " on koulu";
+
+Console.WriteLine(name + rnd_str);
+Console.WriteLine($"{name}{rnd_str}");
+Console.WriteLine("{name}{rnd_str}");
+
+bool isAlive = true; // false
+isAlive = false;
+Console.WriteLine(isAlive);
+
+// ei toimi, koska tyyppiyhteensopivuus
+// isAlive = 1;
+
+char c = 'd';
