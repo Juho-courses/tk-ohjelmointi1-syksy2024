ca73edc1c9fd11d150a46698a812e24187fe52ef
Juho S <juho.salli@gmail.com>
2024-10-17 13:49:13 +0300

    lisää ehtolauseharkka04 esimerkkikoodin

---
+++ harjoitukset/02-ehdot/EhtolauseHarjoitus04/Program.cs
@@ -0,0 +1,18 @@
+﻿int value = 101;
+
+if (value < 100)
+{
+    Console.WriteLine("arvo on aika pieni");
+}
+else if (value == 100)
+{
+    Console.WriteLine("arvo on sata");
+}
+else if (value > 100 && value <= 200)
+{
+    Console.WriteLine("nyt alkaa olla aika iso luku");
+}
+else if (value > 200)
+{
+    Console.WriteLine("nyt ollaa isoissa luvuissa");
+}
