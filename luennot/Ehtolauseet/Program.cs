e769caf04ba3c8183b1c75ed4711dba6bc79254c
Juho S <juho.salli@gmail.com>
2024-10-17 13:26:41 +0300

    ehtolauseluentokoodit

---
+++ luennot/Ehtolauseet/Program.cs
@@ -0,0 +1,36 @@
+﻿// bool valotPaalla = false;
+
+// if (valotPaalla == true)
+// {
+//     valotPaalla = false;
+//     Console.WriteLine("Valot sammutettiin.");
+// }
+// else
+// {
+//     valotPaalla = true;
+//     Console.WriteLine("Valot sytytettiin.");
+// }
+
+
+// int number = 3;
+
+// if (number <= 1)
+// {
+//     Console.WriteLine("eka osuma");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("kakkone");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("kolmone");
+// }
+// else
+// {
+//     Console.WriteLine("jotain muuta");
+// }
+
+Console.Write("Nimesi: ");
+string arvo = Console.ReadLine();
+Console.WriteLine("Moi " + arvo);
