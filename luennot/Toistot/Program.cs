13b10303253ff85b68bfe80281597bf9f741ea37
Juho S <juho.salli@gmail.com>
2024-11-04 11:33:22 +0200

    luentokoodit: toistot

---
+++ luennot/Toistot/Program.cs
@@ -0,0 +1,23 @@
+﻿// for (int i = 20; i >= 0; i--)
+
+// {
+//     Console.WriteLine(i);
+// }
+
+// int[] nums = { 1, 2, 3, 4, 5, 6, 7, 9, 123, 321 };
+
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     if (nums[i] == 4)
+//     {
+//         continue;
+//     }
+//     Console.Write(nums[i] + " ");
+// }
+
+int count = 0;
+while (count < 3)
+{
+    Console.WriteLine(count);
+    count++;
+}
