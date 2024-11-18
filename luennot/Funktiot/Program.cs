30386dda84787d3a73799e47e47bf0733ae6c56d
Juho S <juho.salli@gmail.com>
2024-11-18 11:41:27 +0200

    funktiot luentokoodit

---
+++ luennot/Funktiot/Program.cs
@@ -0,0 +1,9 @@
+﻿int TulostaSanaJaPalautaSenPituus(string sana, bool palautaPituus)
+{
+    if (!palautaPituus) { return 0; }
+    Console.WriteLine("tulostetaan: " + sana);
+    return sana.Length;
+}
+Console.WriteLine(TulostaSanaJaPalautaSenPituus("koira", true));
+int pituus = TulostaSanaJaPalautaSenPituus("kissa", false);
+Console.WriteLine(pituus);
