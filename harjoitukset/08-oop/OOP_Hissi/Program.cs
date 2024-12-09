ae49b5f16ae3c036a981b514d8b26092f869b710
Juho S <juho.salli@gmail.com>
2024-12-09 10:49:41 +0200

    oop hissi ja puhelin

---
+++ harjoitukset/08-oop/OOP_Hissi/Program.cs
@@ -0,0 +1,73 @@
+﻿Elevator elevator = new Elevator(5);
+elevator.Start(3);
+elevator.Start(5);
+elevator.Start(-1);
+elevator.Start(6);
+elevator.Start(1);
+
+class Elevator
+{
+
+    private int min_floor = 1;
+    public int FloorCount { get; private set; }
+    public int CurrentFloor { get; private set; }
+    public bool IsMoving { get; private set; }
+
+    public Elevator(int floorCount)
+    {
+        FloorCount = floorCount;
+        IsMoving = false;
+        CurrentFloor = 1;
+    }
+
+    public void Start(int targetFloor)
+    {
+        if (targetFloor == CurrentFloor)
+        {
+            Console.WriteLine("Already at the target floor");
+            return;
+        }
+
+        if (targetFloor > FloorCount || targetFloor < min_floor)
+        {
+            Console.WriteLine("Invalid floor");
+            return;
+        }
+        int direction = 1;
+        if (targetFloor < CurrentFloor)
+        {
+            direction = -1;
+        }
+
+        while (true)
+        {
+            CurrentFloor += direction;
+            PrintLocation();
+            if (CurrentFloor == targetFloor)
+            {
+                Console.WriteLine($"Target floor {targetFloor} reached!");
+                break;
+            }
+        }
+    }
+
+    public void PrintLocation()
+    {
+        string str = "";
+        for (int i = min_floor; i <= FloorCount; i++)
+        {
+            if (i == CurrentFloor)
+            {
+                str += "[";
+            }
+            str += $"{i}";
+            if (i == CurrentFloor)
+            {
+                str += "]";
+            }
+
+            if (i != FloorCount) { str += " "; }
+        }
+        Console.WriteLine(str);
+    }
+}
---
+++ harjoitukset/08-oop/OOP_p/Program.cs
@@ -0,0 +1,41 @@
+﻿Phone p1 = new Phone("Iphone");
+Console.WriteLine(p1.Model);
+p1.Call("12312313");
+p1.EndCall();
+p1.Call("lkjlkajsda");
+p1.EndCall();
+
+class Phone(string model)
+{
+    public string Model { get; private set; } = model;
+    public string Number { get; private set; }
+    public bool CallOnGoing { get; private set; } = false;
+
+
+    public void Call(string number)
+    {
+        if (CallOnGoing)
+        {
+            Console.WriteLine($"Call already on going {number}");
+            return;
+        }
+
+        Number = number;
+        CallOnGoing = true;
+        Console.WriteLine($"Calling number {Number}");
+    }
+
+    public void EndCall()
+    {
+        if (!CallOnGoing)
+        {
+            Console.WriteLine("No call to hang up.");
+            return;
+        }
+
+        CallOnGoing = false;
+        Console.WriteLine("Hanging up");
+        Number = "";
+    }
+
+}
